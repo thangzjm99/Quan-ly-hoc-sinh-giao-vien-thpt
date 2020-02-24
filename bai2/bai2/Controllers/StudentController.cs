83c6954ea6ced8426fde83d72c438bd03021583d
caominhduc <caominhduc1999@gmail.com>
2020-02-24 22:39:14 +0700

    xong module student. xong upload avatar create

---
+++ bai2/bai2/Controllers/StudentController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bai2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            using (DBModel dbModel = new DBModel())
+            {
+                return View(dbModel.students.ToList());
+            }
+            
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            using(DBModel dbModel = new DBModel())
+            {
+                return View(dbModel.students.Where(x => x.id == id).FirstOrDefault());
+            }
+            
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(student student, HttpPostedFileBase uploadImage)
+        {
+           
+            string fileName = Path.GetFileNameWithoutExtension(uploadImage.FileName);
+            string filePath = Path.Combine(Server.MapPath("~/Images"), fileName);
+            filePath = filePath + ".jpg";
+            uploadImage.SaveAs(filePath);
+            student.image = "~/Images/" + fileName;
+            
+
+            using (DBModel dbModel = new DBModel())
+            {
+                
+                dbModel.students.Add(student);
+                dbModel.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+            
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModel dbModel = new DBModel())
+            {
+                return View(dbModel.students.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, student student)
+        {
+            try
+            {
+                using (DBModel dbModel = new DBModel())
+                {
+                    dbModel.Entry(student).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+                    // TODO: Add update logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModel dbModel = new DBModel())
+            {
+                return View(dbModel.students.Where(x => x.id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DBModel dBModel = new DBModel())
+                {
+                    student student = dBModel.students.Where(x => x.id == id).FirstOrDefault();
+                    dBModel.students.Remove(student);
+                    dBModel.SaveChanges();
+                }
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ bai2/bai2/staff.cs
@@ -0,0 +1,34 @@
+namespace bai2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("staff")]
+    public partial class staff
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [StringLength(255)]
+        public string name { get; set; }
+
+        public int? gender { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? birthday { get; set; }
+
+        [Column(TypeName = "text")]
+        public string address { get; set; }
+
+        [Column(TypeName = "timestamp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [MaxLength(8)]
+        public byte[] created_at { get; set; }
+
+        [Column(TypeName = "text")]
+        public string image { get; set; }
+    }
+}
