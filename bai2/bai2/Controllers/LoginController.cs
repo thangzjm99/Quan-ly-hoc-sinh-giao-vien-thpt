fdb55425053844fc788cd54f5b98e3449ab2d509
thangzjm99 <60538008+thangzjm99@users.noreply.github.com>
2020-02-26 22:58:41 +0700

    Xây login

---
+++ bai2/bai2/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+
+namespace bai2.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            string user = Request.Form["Usernametext"];
+            string pass = Request.Form["Passwordtext"];
+            if (user == "admin" && pass == "admin")
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+            else
+            {
+                MessageBox.Show("Wrong id and password");
+
+                return RedirectToAction("Index", "Login", null);
+            }
+        }
+    }
+}
