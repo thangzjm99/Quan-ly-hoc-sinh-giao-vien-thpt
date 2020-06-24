c7546562c03e5269e140b4b1dcaa03e170ad69fd
ThinhNguyen <abbabc2@gmail.com>
2020-06-24 15:54:14 +0700

    guide + insert

--- bai2/bai2/Controllers/HomeController.cs
+++ bai2/bai2/Controllers/HomeController.cs
@@ -13,10 +13,8 @@ namespace bai2.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Guide()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
